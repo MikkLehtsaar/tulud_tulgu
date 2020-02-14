19b3ed6016c23e39ede03546a3f2307715c9afd5
erkileemet <t3@DESKTOP-N4T4NTG.localdomain>
2020-02-14 22:16:22 +0200

    something

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const second = document.querySelector("#b");
 const btnplus = document.querySelector("#plus");
 const btnminus = document.querySelector("#minus");
 const answer = document.querySelector("#answer");
+const answer2 = document.querySelector("#answer2");
 const balance = document.querySelector("balance")
 
 let balance_val = 0;
@@ -19,6 +20,10 @@ function minus() {
     answer.innerText = balance_val;
 }
 
+function answerFunc() {
+    document.write("a")
+}
 //Output things
 btnplus.addEventListener("click", plus);
-btnminus.addEventListener("click", minus);
+btnminus.addEventListener("click", minus, answerFunc);
+answer2.addEventListener("click", answerFunc)
